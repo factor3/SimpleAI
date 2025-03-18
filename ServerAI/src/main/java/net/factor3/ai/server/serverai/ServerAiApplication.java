da4cf0decbc95cbf11ca1c6c0756918ec345cc0b
Robert Brown III <rbrown_3@zoho.com>
2025-03-17 20:54:57 -0400

    V1.0.0: Complete verion

---
+++ ServerAI/src/main/java/net/factor3/ai/server/serverai/ServerAiApplication.java
@@ -0,0 +1,13 @@
+package net.factor3.ai.server.serverai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerAiApplication.class, args);
+    }
+
+}
---
+++ ServerAI/src/test/java/net/factor3/ai/server/serverai/ServerAiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.factor3.ai.server.serverai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
